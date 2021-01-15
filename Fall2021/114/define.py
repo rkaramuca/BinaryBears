b79f12468cf64847032a33ead44fe7885e2b6bfd
Ronald Karamuca <11005050@live.mercer.edu>
2021-01-14 21:45:52 -0500

    removed define2.py added define.py

--- Fall2021/114/define.py
+++ Fall2021/114/define.py
@@ -16,49 +16,46 @@ suf = {
 
 for i in range(int(input())):
 	word = input()
-	out = ""
-	if word[0:4] in pref:
-		preff = word[0:4]
-		out += pref[preff]
-		out += " "
-	elif word[0:3] in pref:
-		preff = word[0:3]
-		out += pref[preff]
-		out += " "
-	elif word[0:2] in pref:
-		if word[0:3] == "pre":
-			preff = word[0:3]
-			out += pref[preff]
-			out += " "
-		if word[0:2] == "re":
-			preff = word[0:2]
-			out += " "
-			out += pref[preff]
-		else:
-			preff = word[0:2]
-			out += pref[preff]
-			out += " "
-	elif word[-4:] in suf:
+	out = "% @ $ #"
+	preL = word[0:4]
+	pL = ""
+	for i in range(len(preL)):
+		pL += preL[i]
+		if pL in pref:
+			if pL == "re":
+				word = word[2:]
+				out = out.replace("#", pref[pL])
+				break
+			else:
+				word = word[i + 1:]
+				out = out.replace("%", pref[pL])
+				break
+
+	if word[-4:] in suf:
 		suff = word[-4:]
-		out += suf[suff]
-		out += " "
-		out += word[0:-3]
-		out += "ing"
+		word = word[0:-4]
+		word += "ing"
+		out = out.replace("%", suf[suff])
 	elif word[-3:] in suf:
 		suff = word[-3:]
-		out += suf[suff]
-		out += " "
-		out += word[0:-3]
+		word = word[0:-3]
+		out = out.replace("@", suf[suff])
 	elif word[-2:] in suf:
 		suff = word[-2:]
-		out += suf[suff] 
-		out += " "
-		out += word[0:-2]
-		out += "s"
+		out = out.replace("@", suf[suff])
+		word = word[0:-2]
+		word += "s"
 	elif word[-1:] in suf:
 		suff = word[-1:]
-		out += suf[suff]
-		out += " "
-		out += word[0:-1]
-		
+		out = out.replace("@", suf[suff])
+		word = word[0:-1]
+
+	out = out.replace('$', word)
 	
+	out = out.replace('%', "")
+	out = out.replace('@', "")
+	out = out.replace('$', "")
+	out = out.replace('#', "")
+	out = out.strip()
+	out = " ".join(out.split())
+	print(out)
